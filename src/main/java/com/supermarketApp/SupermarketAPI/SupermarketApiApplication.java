cc7b78ed9a9d570d354a4c034e3f84a2756bad46
spriyam1221 <spriyam1221@gmail.com>
2022-04-27 09:53:43 +0530

    Added first feature

---
+++ src/main/java/com/supermarketApp/SupermarketAPI/SupermarketApiApplication.java
@@ -0,0 +1,14 @@
+package com.supermarketApp.SupermarketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketApiApplication.class, args);
+		System.out.println("priya");
+	}
+
+}
